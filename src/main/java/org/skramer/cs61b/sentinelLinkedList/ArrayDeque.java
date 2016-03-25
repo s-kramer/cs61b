80bdfe9d09534c1aa9730e23a2cddd7dfa648c19
Sebastian Kramer <sebastian.kramer@wp.pl>
2016-03-25 20:19:25 +0100

    added array size reduction, fixed bug in getElementCountUntilBack, applied some refactoring

--- src/main/java/org/skramer/cs61b/sentinelLinkedList/ArrayDeque.java
+++ src/main/java/org/skramer/cs61b/sentinelLinkedList/ArrayDeque.java
@@ -3,12 +3,15 @@ package org.skramer.cs61b.sentinelLinkedList;
 import java.util.ArrayList;
 import java.util.List;
 
+// todo: add docs
 public class ArrayDeque<T> implements Deque<T> {
+    private final int INITIAL_CAPACITY = 8;
     private static final int RESIZE_FACTOR = 2;
+    private float DOWNSIZE_FACTOR = 0.25f;
     private static final int STARTING_BACK_INDEX = 5;
     private static final int STARTING_FRONT_INDEX = 5;
 
-    private int capacity = 8;
+    private int capacity = INITIAL_CAPACITY;
     private int size = 0;
 
     // package-private for testing
@@ -51,10 +54,11 @@ public class ArrayDeque<T> implements Deque<T> {
 
     private void resizeArray() {
         final int newCapacity = capacity * RESIZE_FACTOR;
-        // The suppression is safe because it's just a resize
-        // and because old array is not exposed to end users
-        @SuppressWarnings("unchecked")
-        T[] result = (T[]) new Object[newCapacity];
+        resizeArray(newCapacity);
+    }
+
+    private void resizeArray(int newCapacity) {
+        T[] result = makeNewArray(newCapacity);
 
         final int elementCountUntilBack = getElementCountUntilBack();
         final int newFrontIndex = newCapacity - elementCountUntilBack;
@@ -67,8 +71,16 @@ public class ArrayDeque<T> implements Deque<T> {
         capacity = newCapacity;
     }
 
+    private T[] makeNewArray(int newCapacity) {
+        // The suppression is safe because it's just a resize
+        // and because old array is not exposed to end users
+        @SuppressWarnings("unchecked")
+        T[] ts = (T[]) new Object[newCapacity];
+        return ts;
+    }
+
     private int getElementCountUntilBack() {
-        return size - frontIndex;
+        return capacity - frontIndex;
     }
 
     @Override
@@ -83,7 +95,23 @@ public class ArrayDeque<T> implements Deque<T> {
         // todo: add downsizing
         if (size == capacity) {
             resizeArray();
+        } else if (size > INITIAL_CAPACITY && (float) size / capacity < DOWNSIZE_FACTOR) {
+            downsizeArray();
         }
+    }
+
+    private void downsizeArray() {
+        final int newCapacity = getMinimalNewCapacity();
+        resizeArray(newCapacity);
+    }
+
+    private int getMinimalNewCapacity() {
+        int result = 1;
+        while (result < size) {
+            result *= 2;
+        }
+
+        return result;
     }
 
     private int getPreviousIndex(int index) {
@@ -98,6 +126,7 @@ public class ArrayDeque<T> implements Deque<T> {
     // todo: simplify this!!
     @Override
     public void insertNode(int n, T value) {
+        checkIndexArgumentSanity(n);
         ensureCapacity();
 
         int index = getIndexOfNthNode(n);
@@ -142,39 +171,52 @@ public class ArrayDeque<T> implements Deque<T> {
 
     @Override
     public T getNodeValue(int n) {
+        checkIndexArgumentSanity(n);
         int index = getIndexOfNthNode(n);
         return array[index];
     }
 
     private int getIndexOfNthNode(int n) {
+        return (frontIndex + n) % capacity;
+    }
+
+    private void checkIndexArgumentSanity(int n) {
         if (n >= size || n < 0) {
             throw new IllegalArgumentException(String.format("%d node requested but list is only %d long", n, size));
         }
-        return (frontIndex + n) % capacity;
     }
 
     @Override
     public void removeFront() {
-        if (size == 0) {
-            throw new IllegalArgumentException("Remove request on empty list");
-        }
+        checkSizeSanity();
+
         array[frontIndex] = null;
         frontIndex = getNextIndex(frontIndex);
         --size;
+
+        ensureCapacity();
     }
 
     @Override
     public void removeBack() {
-        if (size == 0) {
-            throw new IllegalArgumentException("Remove request on empty list");
-        }
+        checkSizeSanity();
         backIndex = getPreviousIndex(backIndex);
         array[backIndex] = null;
         --size;
+
+        ensureCapacity();
     }
 
+    private void checkSizeSanity() {
+        if (size == 0) {
+            throw new IllegalArgumentException("Remove request on empty list");
+        }
+    }
+
+    // todo: simplify this
     @Override
     public void removeNode(int n) {
+        checkIndexArgumentSanity(n);
         int index = getIndexOfNthNode(n);
 
         int srcPos = index + 1;
@@ -203,5 +245,7 @@ public class ArrayDeque<T> implements Deque<T> {
 
         backIndex = getPreviousIndex(backIndex);
         --size;
+
+        ensureCapacity();
     }
 }
--- src/test/java/org/skramer/cs61b/sentinelLinkedList/ArrayDequeTest.java
+++ src/test/java/org/skramer/cs61b/sentinelLinkedList/ArrayDequeTest.java
@@ -7,7 +7,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by skramer on 3/16/16.
@@ -69,5 +73,29 @@ public class ArrayDequeTest extends AbstractDequeTest {
         Deque<Integer> deque = new ArrayDeque<>(integerList);
         assertEquals(20, deque.getSize());
         assertEquals(deque.getValues(), integerList);
+    }
+
+    @Test
+    public void dequeFunctionsAfterDownsizing() {
+        int testElementUpperBound = 128;
+        List<Integer> integerList = IntStream.range(0, testElementUpperBound).boxed().collect(Collectors.toList());
+        ArrayDeque<Integer> deque = new ArrayDeque<>(integerList);
+
+        for (int i = testElementUpperBound - 1; i >= 16; i--) {
+            deque.removeNode(i);
+        }
+
+        assertThat(deque.getSize(), equalTo(16));
+        assertThat(deque.getValues(), is(IntStream.range(0, 16).boxed().collect(Collectors.toList())));
+
+        float arrayActualUsage = getArrayActualUsage(deque);
+        assertThat(String.format("array usage: %f", arrayActualUsage), Float.compare(arrayActualUsage, 0.25f), greaterThanOrEqualTo(0));
+    }
+
+    private float getArrayActualUsage(ArrayDeque<?> list) {
+        Object[] array = list.array;
+        long count = Arrays.stream(array).filter(i -> i != null).count();
+        float usageRatio = (float) count / array.length;
+        return usageRatio;
     }
 }
